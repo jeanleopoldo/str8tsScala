17c5d831732831e15ef0d7bf328583106d597449
Jean Leopoldo <jean@jettacargo.com.br>
2022-02-12 11:27:05 -0300

    chore: removing old files when compiling

--- script/run.sh
+++ script/run.sh
@@ -3,7 +3,11 @@ RELATIVE_PATH="`dirname \"$0\"`"
 SCRIPT_PATH=$(realpath $RELATIVE_PATH)
 PROJECT_PATH=$(realpath $SCRIPT_PATH/..)
 
+
 cd $PROJECT_PATH
+
+rm -rf main.class
+rm -rf main$.class
 
 scalac main.scala
 scala main
