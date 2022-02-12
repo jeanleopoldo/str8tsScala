7e8fe532f332ed88bc43a00cd5746bbb230e4593
Jean Leopoldo <jean@jettacargo.com.br>
2022-02-12 14:14:11 -0300

    chore: removing compiled files from folder

--- script/run.sh
+++ script/run.sh
@@ -3,11 +3,12 @@ RELATIVE_PATH="`dirname \"$0\"`"
 SCRIPT_PATH=$(realpath $RELATIVE_PATH)
 PROJECT_PATH=$(realpath $SCRIPT_PATH/..)
 
-
 cd $PROJECT_PATH
 
-rm -rf main.class
-rm -rf main$.class
+reset
 
 scalac main.scala
 scala main
+
+rm -rf main.class
+rm -rf main$.class
